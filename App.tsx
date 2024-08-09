b99d8cb4ceeb98976f9cd304685d832cc5018caf
fmacedosantos <felipemacedo986@gmail.com>
2024-08-09 08:50:50 -0300

    Eventos de gestos para a tag View

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View
+        onTouchStart={(evento) => {
+          Alert.alert('TOQUE', 'Clique iniciado')
+        }}
+        onTouchEnd={(evento) => {
+          Alert.alert('TOQUE', 'Clique finalizado')
+        }}
+      >
+        <Text>CLIQUE</Text>
+      </View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
